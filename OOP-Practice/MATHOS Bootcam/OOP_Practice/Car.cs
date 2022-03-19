24a4017d50e684bde558df1db95ccdaa01803dba
Ivan Husnjak <husnjak.ivan@gmail.com>
2022-03-19 20:24:43 +0100

    OOP practice (polymorphism, inheritance....)

---
+++ OOP-Practice/MATHOS Bootcam/OOP_Practice/Car.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_Practice
+{
+    public class Car : Vehicle
+    {
+        public Car(string carEngine, string carNumOfSeats, string vehicleColor, string vehicleModel) 
+            : base(vehicleColor, vehicleModel)
+        {
+            CarEngine = carEngine;
+            CarNumOfSeats = carNumOfSeats;
+        }
+
+        public string CarEngine { get; set; }
+        public string CarNumOfSeats { get; set; }
+
+        public override string PrintVehicleInfo()
+        {
+            return "\n----------" + this.VehicleModel + "\n" + this.VehicleColor + "\n"
+                + this.VehicleProductionDate + "\n" + this.VehicleVinNumber
+                + "\n" + this.CarEngine + "\n" + this.CarNumOfSeats + "\n----------";
+        }
+    }
+}
---
+++ OOP-Practice/MATHOS Bootcam/OOP_Practice/IVehicle.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_Practice
+{
+    internal interface IVehicle
+    {
+        string VehicleColor { get; set; }
+        string VehicleModel { get; set; }
+        Guid VehicleVinNumber { get; set; }
+        DateTime VehicleProductionDate { get; set; }
+    }
+}
